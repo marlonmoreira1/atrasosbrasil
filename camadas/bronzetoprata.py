155d92637dec1bade1483c38efa61711d5864a9e
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-09-25 18:36:36 -0300

    Update bronzetoprata.py

--- camadas/bronzetoprata.py
+++ camadas/bronzetoprata.py
@@ -12,7 +12,7 @@ from bs4 import BeautifulSoup
 from webdriver_manager.chrome import ChromeDriverManager
 from azure.storage.blob import BlobServiceClient
 from unidecode import unidecode
-
+from movedata import save, read
 
 options = Options()
 options.add_argument('--headless')  
@@ -95,42 +95,7 @@ bronze_container = os.environ['CONTAINER_NAME']
 prata_container = os.environ['CONTAINER_PRATA']
 
 
-def read_from_bronze():
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    container_client = blob_service_client.get_container_client(bronze_container)
-
-    data_hoje = datetime.today()
-    data_ontem = data_hoje - timedelta(days=1)
-    data_filtro = data_ontem.strftime('%Y-%m-%d')
-
-    blob_client = container_client.get_blob_client(f"voos_{data_filtro}_bronze.parquet")
-    stream = blob_client.download_blob()
-    data = stream.readall()
-
-    parquet_buffer = BytesIO(data)
-    df = pd.read_parquet(parquet_buffer)
-
-    return df
-
-
-def save_to_prata(df):
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    container_client = blob_service_client.get_container_client(prata_container)
-    
-    parquet_buffer = BytesIO()
-    df.to_parquet(parquet_buffer, index=False)
-    parquet_data = parquet_buffer.getvalue()
-    
-    data_hoje = datetime.today()
-    data_ontem = data_hoje - timedelta(days=1)
-    data_filtro = data_ontem.strftime('%Y-%m-%d')
-
-    blob_name = f"voos_{data_filtro}_prata.parquet"
-    blob_client = container_client.get_blob_client(blob_name)
-    blob_client.upload_blob(parquet_data, overwrite=True)
-
-
-voos = read_from_bronze()
+voos = read(bronze_container)
 
 
 voos[['Status', 'Hora_realizada', 'AM-PM_Realizado']] = voos['Status'].str.extract(r'([a-zA-Z\s\.]+)(\d{1,2}:\d{2})?\s?(AM|PM)?')
@@ -161,5 +126,5 @@ def obter_informacoes_geograficas(cidade):
 voos[['Cidade_Correta', 'Estado/Província', 'País']] = voos['From'].apply(lambda x: pd.Series(obter_informacoes_geograficas(x)))
 
 
-save_to_prata(voos)
+save(voos,prata_container)
 
