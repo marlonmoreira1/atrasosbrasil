f86cd60a35b1a6c79a5c81414627ecbc08ecedc6
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-09-25 20:48:38 -0300

    Update movedata.py

--- camadas/movedata.py
+++ camadas/movedata.py
@@ -6,7 +6,7 @@ from io import BytesIO
 
 
 
-def read(connect_str,container):
+def read(connect_str,container,nome):
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     container_client = blob_service_client.get_container_client(container)
 
@@ -14,7 +14,7 @@ def read(connect_str,container):
     data_ontem = data_hoje - timedelta(days=1)
     data_filtro = data_ontem.strftime('%Y-%m-%d')
 
-    blob_client = container_client.get_blob_client(f"voos_{data_filtro}_prata.parquet")
+    blob_client = container_client.get_blob_client(nome)
     stream = blob_client.download_blob()
     data = stream.readall()
 
@@ -24,7 +24,7 @@ def read(connect_str,container):
     return df
 
 
-def save(df,connect_str,container):
+def save(df,connect_str,container,nome):
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     container_client = blob_service_client.get_container_client(container)
     
@@ -36,6 +36,6 @@ def save(df,connect_str,container):
     data_ontem = data_hoje - timedelta(days=1)
     data_filtro = data_ontem.strftime('%Y-%m-%d')
 
-    blob_name = f"voos_{data_filtro}_prata.parquet"
+    blob_name = nome
     blob_client = container_client.get_blob_client(blob_name)
     blob_client.upload_blob(parquet_data, overwrite=True)
