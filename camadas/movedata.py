2bdc2e2cc7bc7bcde7f317e0150e92db7317a073
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-09-25 21:09:04 -0300

    Update movedata.py

--- camadas/movedata.py
+++ camadas/movedata.py
@@ -14,7 +14,7 @@ def read(connect_str,container,nome):
     data_ontem = data_hoje - timedelta(days=1)
     data_filtro = data_ontem.strftime('%Y-%m-%d')
 
-    blob_name = f"voos_{data_filtro}_.parquet" + nome
+    blob_name = f"voos_{data_filtro}_{nome}.parquet"
 
     blob_client = container_client.get_blob_client(blob_name)
     stream = blob_client.download_blob()
@@ -38,6 +38,6 @@ def save(df,connect_str,container,nome):
     data_ontem = data_hoje - timedelta(days=1)
     data_filtro = data_ontem.strftime('%Y-%m-%d')
 
-    blob_name = f"voos_{data_filtro}_.parquet" + nome
+    blob_name = f"voos_{data_filtro}_{nome}.parquet"
     blob_client = container_client.get_blob_client(blob_name)
     blob_client.upload_blob(parquet_data, overwrite=True)
