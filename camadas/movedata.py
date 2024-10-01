ff5a0db4ea89f26eec547e6310def02491744817
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-10-01 08:20:22 -0300

    Update movedata.py

--- camadas/movedata.py
+++ camadas/movedata.py
@@ -10,9 +10,8 @@ def read(connect_str,container,nome):
     blob_service_client = BlobServiceClient.from_connection_string(connect_str)
     container_client = blob_service_client.get_container_client(container)
 
-    data_hoje = datetime.today()
-    data_ontem = data_hoje - timedelta(days=1)
-    data_filtro = data_ontem.strftime('%Y-%m-%d')
+    
+    data_filtro = '2024-09-27'
 
     blob_name = f"voos_{data_filtro}_{nome}.parquet"
 
@@ -32,11 +31,9 @@ def save(df,connect_str,container,nome):
     
     parquet_buffer = BytesIO()
     df.to_parquet(parquet_buffer, index=False)
-    parquet_data = parquet_buffer.getvalue()
+    parquet_data = parquet_buffer.getvalue()    
     
-    data_hoje = datetime.today()
-    data_ontem = data_hoje - timedelta(days=1)
-    data_filtro = data_ontem.strftime('%Y-%m-%d')
+    data_filtro = '2024-09-27'
 
     blob_name = f"voos_{data_filtro}_{nome}.parquet"
     blob_client = container_client.get_blob_client(blob_name)
