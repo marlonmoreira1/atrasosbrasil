7166153ef8796ca14c9d35eaa33d979b086778d6
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-10-01 13:21:43 -0300

    Update pratatoouro.py

--- camadas/pratatoouro.py
+++ camadas/pratatoouro.py
@@ -16,46 +16,36 @@ prata_container = os.environ['CONTAINER_PRATA']
 
 voos = read(connect_str,prata_container,"prata")
 
-voos['Airline'] = voos['Airline'].str.replace(r'\s*\(.*?\)-', '', regex=True)
-voos['Airline'] = voos['Airline'].str.replace(r'\-$', '', regex=True)
+voos['Companhia_Aerea'] = voos['Airline'].str.replace(r'\s*\(.*?\)-', '', regex=True)
+voos['Companhia_Aerea'] = voos['Companhia_Aerea'].str.replace(r'\-$', '', regex=True)
 
-voos[['Status', 'Hora_realizada', 'AM-PM_Realizado']] = voos['Status'].str.extract(r'([a-zA-Z\s\.]+)(\d{1,2}:\d{2})?\s?(AM|PM)?')
-voos[['Time', 'AM-PM_Previsto']] = voos['Time'].str.extract(r'(\d{1,2}:\d{2})\s?(AM|PM)')
+voos[['Aeronave', 'Modelo_Aeronave']] = voos['Aircraft'].str.extract(r'(.+)\((.+)\)')
 
-voos[['Aircraft', 'Aircraft_type']] = voos['Aircraft'].str.extract(r'(.+)\((.+)\)')
+voos[['Status', 'Hora_Realizada', 'AM-PM_Realizado']] = voos['Status'].str.extract(r'([a-zA-Z\s\.]+)(\d{1,2}:\d{2})?\s?(AM|PM)?')
+voos[['Hora_Prevista', 'AM-PM_Previsto']] = voos['Time'].str.extract(r'(\d{1,2}:\d{2})\s?(AM|PM)')
 
 def obter_nacionalidade(row):
     if row != 'Brazil':
         return 'Internacional'
     return 'Nacional'
 
-voos['Is_National'] = voos['country'].apply(obter_nacionalidade)
+voos['Tipo_Voo_Nacional'] = voos['country'].apply(obter_nacionalidade)
 
 voos['Voo_Id'] = voos['Flight'] + voos['date_flight'] + voos['Aeroporto'] + voos['Aircraft']
 
-colunas_traduzidas = {
-    'Time': 'Hora_Prevista',
-    'Flight': 'Numero_Voo',    
-    'Airline': 'Companhia_Aerea',
-    'Aircraft': 'Modelo_Aeronave',
-    'Status': 'Status_Voo',
+voos = voos.drop(columns=['Time','Airline','Aircraft'])
+
+colunas_para_renomear = {
+    'Flight': 'Numero_Voo',
+    'Status': 'Status_voo',
     'Delay_status': 'Indicador_Atraso_Cor',
     'date_flight': 'Data_Voo',
-    'direcao': 'Direcao_Voo',
-    'Aeroporto': 'Nome_Aeroporto',
-    'Aircraft_type': 'Tipo_Aeronave',
-    'Hora_realizada': 'Hora_Realizada',
-    'admin_name': 'Estado',
-    'country': 'Pais',
-    'Is_National': 'Tipo_Voo_Nacional',
     'city_normalized': 'Cidade_Normalizada',
     'city': 'Cidade_ascii',
-    'AM-PM_Previsto': 'Periodo_Previsto',
-    'AM-PM_Realizado': 'Periodo_Realizado'   
-    
+    'admin_name': 'Estado',
+    'country': 'Pais'
 }
 
-voos = voos.rename(columns=colunas_traduzidas)
-
+voos = voos.rename(columns=colunas_para_renomear)
 
 save(voos,connect_str,ouro_container,"ouro")
