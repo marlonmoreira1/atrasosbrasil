42879002d578894ddeb138616551b913b88d87ac
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2024-11-06 10:30:29 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -49,7 +49,7 @@ def obter_voos(url):
 
     while True:
         try:
-            load_more_button = WebDriverWait(driver, 20).until(
+            load_more_button = WebDriverWait(driver, 25).until(
                     EC.element_to_be_clickable((By.XPATH, "//button[@class='btn btn-table-action btn-flights-load']")))
                     
             if load_more_button and load_more_button.is_displayed() and load_more_button.is_enabled():
@@ -65,7 +65,7 @@ def obter_voos(url):
     time.sleep(1)
   
     element = WebDriverWait(driver, 50).until(
-        EC.presence_of_all_elements_located((By.XPATH, "//table[contains(@class, 'table-condensed') and contains(@class, 'table-hover') and contains(@class, 'data-table')]"))
+        EC.presence_of_element_located((By.XPATH, "//table[contains(@class, 'table-condensed') and contains(@class, 'table-hover') and contains(@class, 'data-table')]"))
     )      
 
     html_content = element.get_attribute('outerHTML')
