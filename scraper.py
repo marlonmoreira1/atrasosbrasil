726730e0b84f0414d4d24a79c0a2919703124f5c
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2025-02-15 11:30:19 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -23,7 +23,7 @@ from selenium.webdriver.chrome.options import Options
 from io import BytesIO
 
 options = Options()
-options.add_argument('--headless')  
+#options.add_argument('--headless')  
 options.add_argument('--no-sandbox')
 options.add_argument('--disable-dev-shm-usage')
 options.page_load_strategy = "normal"
