f551fae5a61851d6f290e65c9b3e2119b3ca5d29
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2026-05-28 16:38:16 -0300

    Update pratatoouro.py

--- camadas/pratatoouro.py
+++ camadas/pratatoouro.py
@@ -1,48 +1,41 @@
 import os
-import time
 import pandas as pd
-from datetime import datetime, timedelta
-from azure.storage.blob import BlobServiceClient
-from unidecode import unidecode
-from movedata import save, read
+from movedata import read, save
 
-connect_str = os.environ['CONNECT_STR']
-ouro_container = os.environ['CONTAINER_OURO']
-prata_container = os.environ['CONTAINER_PRATA']
+CONNECT_STR = os.environ["CONNECT_STR"]
 
+PRATA = os.environ["CONTAINER_PRATA"]
+OURO = os.environ["CONTAINER_OURO"]
 
-voos = read(connect_str,prata_container,"prata")
+df = read(CONNECT_STR, PRATA, "prata")
 
-voos['Companhia_Aerea'] = voos['Airline'].str.replace(r'\s*\(.*?\)', '', regex=True)
-voos['Companhia_Aerea'] = voos['Companhia_Aerea'].str.replace(r'\-$', '', regex=True)
+def calculate_status(row):
 
-voos[['Aeronave', 'Modelo_Aeronave']] = voos['Aircraft'].str.extract(r'(.+)\((.+)\)')
+    if pd.isna(row["lastSeen"]):
+        return "Unknown"
 
-voos[['Status', 'Hora_Realizada', 'AM-PM_Realizado']] = voos['Status'].str.extract(r'([a-zA-Z\s\.]+)(\d{1,2}:\d{2})?\s?(AM|PM)?')
-voos[['Hora_Prevista', 'AM-PM_Previsto']] = voos['Time'].str.extract(r'(\d{1,2}:\d{2})\s?(AM|PM)')
+    return "Completed"
 
-def obter_nacionalidade(row):
-    if row != 'Brazil':
-        return 'Internacional'
-    return 'Nacional'
+df["Status"] = df.apply(
+    calculate_status,
+    axis=1
+)
 
-voos['Tipo_Voo_Nacional'] = voos['country'].apply(obter_nacionalidade)
+df["Duracao_Minutos"] = (
+    (df["lastSeen"] - df["firstSeen"]) / 60
+)
 
-voos['Voo_Id'] = voos['Flight'] + voos['date_flight'] + voos['Aeroporto'] + voos['Aircraft'] + voos['city_normalized'] + voos['Status']
+df = df.rename(columns={
+    "Flight": "Numero_Voo",
+    "Airline": "Companhia_Aerea",
+    "date_flight": "Data_Voo"
+})
 
-voos = voos.drop(columns=['Time','Airline','Aircraft'])
+save(
+    df,
+    CONNECT_STR,
+    OURO,
+    "ouro"
+)
 
-colunas_para_renomear = {
-    'Flight': 'Numero_Voo',
-    'Status': 'Status_voo',
-    'Delay_status': 'Indicador_Atraso_Cor',
-    'date_flight': 'Data_Voo',
-    'city_normalized': 'Cidade_Normalizada',
-    'city': 'Cidade_ascii',
-    'admin_name': 'Estado',
-    'country': 'Pais'
-}
-
-voos = voos.rename(columns=colunas_para_renomear)
-
-save(voos,connect_str,ouro_container,"ouro")
+print(df.head())
