0b534e514273f181cdc40fe1c13cdec9d55b5e31
Márlon Moreira de Almeida <71144665+marlonmoreira1@users.noreply.github.com>
2026-05-28 16:33:46 -0300

    Update movedata.py

--- camadas/movedata.py
+++ camadas/movedata.py
@@ -1,45 +1,51 @@
+```python
 from azure.storage.blob import BlobServiceClient
-import pandas as pd
-from datetime import datetime, timedelta
-import os
 from io import BytesIO
+from datetime import datetime, timedelta
+import pandas as pd
 
+def read(connect_str, container, camada):
 
+    blob_service_client = BlobServiceClient.from_connection_string(
+        connect_str
+    )
 
-def read(connect_str,container,nome):
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    container_client = blob_service_client.get_container_client(container)
+    container_client = blob_service_client.get_container_client(
+        container
+    )
 
-    
-    data_hoje = datetime.today()
-    data_ontem = data_hoje - timedelta(days=1)
-    data_filtro = data_hoje.strftime('%Y-%m-%d')    
-    
-    blob_name = f"voos_{data_filtro}_{nome}.parquet"
+    yesterday = datetime.utcnow() - timedelta(days=1)
+
+    blob_name = f"voos_{yesterday.strftime('%Y-%m-%d')}_{camada}.parquet"
 
     blob_client = container_client.get_blob_client(blob_name)
-    stream = blob_client.download_blob()
-    data = stream.readall()
 
-    parquet_buffer = BytesIO(data)
-    df = pd.read_parquet(parquet_buffer)
+    data = blob_client.download_blob().readall()
 
-    return df
+    return pd.read_parquet(BytesIO(data))
 
+def save(df, connect_str, container, camada):
 
-def save(df,connect_str,container,nome):
-    blob_service_client = BlobServiceClient.from_connection_string(connect_str)
-    container_client = blob_service_client.get_container_client(container)
-    
-    parquet_buffer = BytesIO()
-    df.to_parquet(parquet_buffer, index=False)
-    parquet_data = parquet_buffer.getvalue()    
-    
-    data_hoje = datetime.today()
-    data_ontem = data_hoje - timedelta(days=1)
-    data_filtro = data_hoje.strftime('%Y-%m-%d')
-    
+    blob_service_client = BlobServiceClient.from_connection_string(
+        connect_str
+    )
 
-    blob_name = f"voos_{data_filtro}_{nome}.parquet"
+    container_client = blob_service_client.get_container_client(
+        container
+    )
+
+    yesterday = datetime.utcnow() - timedelta(days=1)
+
+    blob_name = f"voos_{yesterday.strftime('%Y-%m-%d')}_{camada}.parquet"
+
+    buffer = BytesIO()
+
+    df.to_parquet(buffer, index=False)
+
     blob_client = container_client.get_blob_client(blob_name)
-    blob_client.upload_blob(parquet_data, overwrite=True)
+
+    blob_client.upload_blob(
+        buffer.getvalue(),
+        overwrite=True
+    )
+```
